e91a44f2227e991ccab8f377e9f9f0a64edbf9a0
PAPPURAJ <pappuraj.duet@gmail.com>
2023-06-20 11:15:30 +0600

    dao

--- src/main/java/io/github/pappuraj/JDBCPractice/App.java
+++ src/main/java/io/github/pappuraj/JDBCPractice/App.java
@@ -1,9 +1,5 @@
 package io.github.pappuraj.JDBCPractice;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 /**
  * Hello world!
  *
@@ -14,14 +10,9 @@ public class App
     {
 
 
-    	ApplicationContext context=new ClassPathXmlApplicationContext("io/github/pappuraj/JDBCPractice/config.xml");
-    	JdbcTemplate template=context.getBean("myJDBC",JdbcTemplate.class);
 
-    	String query="INSERT INTO engineers(name,position,fieldOfWork,salary) VALUES(?,?,?,?)";
-    	
-    	int val=template.update(query,"Shayeri","Junior","Native",20000);
-   
-    	
-    	System.out.println("Row effected: "+val);
     }
+    
+    
+    
 }
